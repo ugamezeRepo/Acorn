92a752a29a347b9c5be5c71af9bc1cb7fa83de37
ugameze <ugameze@gmail.com>
2023-10-27 17:43:02 +0900

    D+13 pm java 클래스

---
+++ acorn-java/Step03_Class/src/test/main/MainClass04.java
@@ -0,0 +1,15 @@
+package test.main;
+
+import test.mypac.MyUtil;
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		// MyUtil class의 static method 호출
+		MyUtil.send();
+
+		// MyUtil class의 static field 호출
+		String version = MyUtil.version;
+		System.out.println(version);
+
+	}
+}
