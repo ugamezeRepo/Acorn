84b214ad26fbe260edfcd5276dad0e456dc8e9b0
ugameze <ugameze@gmail.com>
2023-10-27 15:08:40 +0900

    정리

--- HelloJava/src/test/main/MainClass.java
+++
@@ -1,8 +0,0 @@
-package test.main;
-
-public class MainClass {
-	// run 했을 때 실행의 흐름이 시작되는 특별한 main 메소드 (함수)
-	public static void main(String[] args) {
-		System.out.println("Hello, World!"); // 출력문eE
-	}
-}
---
+++ acorn-java/Step01_DataType/src/test/main/MainClass06.java
@@ -0,0 +1,16 @@
+package test.main;
+
+@SuppressWarnings("unused")
+public class MainClass06 {
+	public static void main(String[] args) {
+		System.out.println("main 메서드가 시작되었습니다.");
+		// 지역 변수를 선언만 하면? 만들어지지 않는다.
+		int num;
+		String name1;
+
+		// int result = 10 + num;
+		// System.out.println(name1);
+
+		System.out.println("main 메서드가 종료됩니다.");
+	}
+}
